d10596a65212a7f375121aaeaa9e9738ace39366
Jay Stakelon <stakes@users.noreply.github.com>
2020-12-05 17:26:15 -0800

    rename things

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,48 +1,92 @@
 import * as React from 'react';
+import {useState, useEffect, useRef} from 'react';
+import Searchbar from './Searchbar';
+import Pillters from './Pillters';
 import '../styles/ui.css';
 
-declare function require(path: string): any;
+export default function App() {
+    const APIKEY = 'vqixbX9GqzUQGbZCg9e0az1t6ic7h0hd';
 
-const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
+    const [searchString, setSearchString] = useState('');
+    // const [loaderLink, setLoaderLink] = useState('https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif');
+    const [stickers, setStickers] = useState([]);
 
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
+    const onPlaceSticker = (stickerObj) => {
+        let url = stickerObj.url;
+        fetch(url)
+            .then((r) => r.arrayBuffer())
+            .then((a) =>
+                parent.postMessage(
+                    {pluginMessage: {type: 'place-sticker', data: new Uint8Array(a), title: stickerObj.title}},
+                    '*'
+                )
+            )
+            .catch((err) => console.error({err}));
+    };
 
-    const onCreate = React.useCallback(() => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    }, []);
+    const runSearch = (v) => {
+        setSearchString(v);
+    };
 
-    const onCancel = React.useCallback(() => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    }, []);
+    const updateSearchString = (str) => {
+        setSearchString(str);
+    };
 
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const { type, message } = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            };
+    let isFirstRun = useRef(true);
+    useEffect(() => {
+        if (isFirstRun.current === true) {
+            isFirstRun.current = false;
+            return;
         }
+        let url = `https://api.giphy.com/v1/stickers/search?api_key=${APIKEY}&q=${searchString}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((stickerArray) => {
+                let stickers = [];
+                stickerArray.data.forEach((el) => {
+                    stickers.push({url: el.images['original_still'].url, title: el.title});
+                });
+                setStickers(stickers);
+            });
+    }, [searchString]);
+
+    // get trending stickers once initially
+    useEffect(() => {
+        let url = `https://api.giphy.com/v1/stickers/trending?api_key=${APIKEY}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((stickerArray) => {
+                let stickers = [];
+                stickerArray.data.forEach((el) => {
+                    stickers.push({url: el.images['original_still'].url, title: el.title});
+                });
+                setStickers(stickers);
+            });
     }, []);
 
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
+            <Searchbar onSearch={updateSearchString} searchString={searchString}></Searchbar>
+            <Pillters onClick={(v) => runSearch(v)} />
+            {!stickers.length && !isFirstRun.current && (
+                // <image src="" alt="" ></image>
+                <p>
+                    No stickers matching <em>{searchString}</em>
+                </p>
+            )}
+            <div className="stickerList">
+                {stickers.map((value) => (
+                    <div key={value.url} className="stickerContainer">
+                        <img
+                            width={128}
+                            key={value.url}
+                            src={value.url}
+                            alt="gif"
+                            onClick={() => onPlaceSticker(value)}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
-};
-
-export default App;
+}
