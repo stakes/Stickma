849bd736268320e07e52df52b79f7fa22b5c34cb
Jay Stakelon <stakes@users.noreply.github.com>
2020-12-27 16:21:52 -0800

    lil style tweaks

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -5,6 +5,7 @@ figma.ui.onmessage = (msg) => {
     let anchorY = figma.viewport.center.y;
     let zoom = figma.viewport.zoom;
     let s = 1 / zoom;
+
     let selection = figma.currentPage.selection[0];
     if (selection) {
         anchorX = selection.absoluteTransform[0][2] + selection.width;
@@ -12,27 +13,11 @@ figma.ui.onmessage = (msg) => {
     }
 
     if (msg.type === 'place-sticker') {
-        // const stickerOpacity = true ? 0 : 1
-        // const frame = figma.createFrame();
-        // frame.x = anchorX - frame.width / 2;
-        // frame.y = anchorY - frame.height / 2;
-        // frame.horizontalPadding = frame.verticalPadding = 8;
-        // frame.layoutMode = 'HORIZONTAL';
-        // frame.primaryAxisSizingMode = 'AUTO';
-        // frame.counterAxisSizingMode = 'AUTO';
-        // frame.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}, opacity: stickerOpacity}];
-        // frame.strokeAlign = 'INSIDE';
-        // frame.strokeWeight = 1;
-        // frame.strokes = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}, opacity: 0.15}];
-        // frame.effects = [bevelEffect, shadowEffect]
-
         const imageFrame = figma.createFrame();
-        imageFrame.resizeWithoutConstraints(100 * s, 100 * s);
+        imageFrame.resizeWithoutConstraints(150 * s, 150 * s);
         imageFrame.fills = makeFillFromImageData(msg.data);
-        imageFrame.x = anchorX - imageFrame.width / 2;
-        imageFrame.y = anchorY - imageFrame.height / 2;
-
-        // frame.appendChild(imageFrame);
+        imageFrame.x = anchorX;
+        imageFrame.y = anchorY;
 
         const group = figma.group([imageFrame], figma.currentPage);
         group.name = msg.title ? msg.title : 'Sticker';
